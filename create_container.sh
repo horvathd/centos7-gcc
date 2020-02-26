e5bfa98d3f440795b82dc951f93721e5f3a186f4
David Horvath <github@horvathd.eu>
2020-02-26 11:31:13 +0100

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 0 ]; then
-    echo "Compiling default gcc"
     gcc_version=9.2.0
 else
     gcc_version=$1
-    echo "Comiling gcc ${gcc_version}"
 fi
 
-echo "podman build -t centos7-gcc${gcc_version} --build-arg GCC_VERSION=${gcc_version} ."
+echo "Updating centos7 image"
+podman pull centos:7
+
+echo "Building centos7-gcc:${gcc_version} image"
 podman build -t centos7-gcc:${gcc_version} --build-arg GCC_VERSION=${gcc_version} .
